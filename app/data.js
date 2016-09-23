3653c7c02131ffdccbdeccaa6bdaba5481a8cdc6
Brian So <briansostudio@gmail.com>
2016-09-23 14:34:44 +0800

    add back data.js

---
+++ app/data.js
@@ -0,0 +1,105 @@
+export default {
+    info:[
+        ["Major","Computer Science"],
+        ["University","HKUST","http://ust.hk"],
+        ["E-MAIL","khbso@connect.ust.hk","mailto:khbso@connect.ust.hk"],
+        ["Website","ihome.ust.hk/~khbso","http://ihome.ust.hk/~khbso"]
+    ],
+    educations:[
+        {
+            period:"2015 - Now",
+            institution:"Hong Kong University of Science and Technology",
+            program:"BEng in Computer Science",
+            description:"The Hong Kong University of Science and Technology (HKUST) is a public research university in Clear Water Bay Peninsula, Hong Kong. Established in 1991, it is the territory's youngest higher learning institution with no precursory existence."
+        },
+        {
+            period:"2013 - 2015",
+            institution:"Community College of City University of Hong Kong",
+            program:"ASc in Information System Developerment",
+            description:"The Community College of City University is one of the community colleges in Hong Kong. CCCU was established in 2004 under the City University of Hong Kong (CityU) and is now providing about 6,000 places of associate degree programmes provided by four different divisions."
+        }
+    ],
+    experiences:[
+        {
+            period:"2015 - Now",
+            company:"BrianSo Studio",
+            title:"Chairman",
+            description:"BrianSo Studio is an organization that focus on Itaque earum rerum hic tenetur a sapiente delectus reiciendis maiores alias Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        },
+        {
+            period:"2014 - 2015",
+            company:"Google Inc.",
+            title:"Software Engineer",
+            description:"Develop some experimental projects using cutting edged technology. Itaque earum rerum hic tenetur a sapiente delectus reiciendis maiores alias Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        },
+        {
+            period:"2013 - 2014",
+            company:"Oracle Inc.",
+            title:"Web App Developer",
+            description:"Develop the web app using cutting edged technology. Itaque earum rerum hic tenetur a sapiente delectus reiciendis maiores alias Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        }
+    ],
+    skillSections:[
+        {
+            name:"Languages",
+            icon:"glyphicon-cog",
+            skills:[
+                ["Javascript(ES6, Node.js)","95%"],
+                ["CSS3","85%"],
+                ["Java","85%"],
+                ["C++","70%"],
+                ["Objective-C","70%"],
+                ["C#","60%"],
+                ["PHP","55%"]
+            ]
+        },
+        {
+            name:"Professional Skills",
+            icon:"glyphicon-briefcase",
+            skills:[
+                ["Communication","90%"],
+                ["Project Management","90%"],
+                ["Leadership","85%"],
+                ["Confidence","85%"],
+            ]
+        }
+    ],
+    works:[
+        {
+            img:"/public/images/g1.jpg",
+            description:"Lorem Ipsum is simply dummy the when an unknown galley"
+        },
+        {
+            img:"/public/images/g2.jpg",
+            description:"Lorem Ipsum is simply dummy the when an unknown galley"
+        },
+        {
+            img:"/public/images/g3.jpg",
+            description:"Lorem Ipsum is simply dummy the when an unknown galley"
+        },
+        {
+            img:"/public/images/g4.jpg",
+            description:"Lorem Ipsum is simply dummy the when an unknown galley"
+        },
+        {
+            img:"/public/images/g5.jpg",
+            description:"Lorem Ipsum is simply dummy the when an unknown galley"
+        },
+        {
+            img:"/public/images/g6.jpg",
+            description:"Lorem Ipsum is simply dummy the when an unknown galley"
+        },
+        {
+            img:"/public/images/g3.jpg",
+            description:"Lorem Ipsum is simply dummy the when an unknown galley"
+        },
+        {
+            img:"/public/images/g2.jpg",
+            description:"Lorem Ipsum is simply dummy the when an unknown galley"
+        },
+        {
+            img:"/public/images/g1.jpg",
+            description:"Lorem Ipsum is simply dummy the when an unknown galley"
+        }
+    ]
+};
