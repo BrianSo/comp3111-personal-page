c4f003d592a80a97b22345d2035627879fac6450
Brian So <briansostudio@gmail.com>
2016-09-24 23:15:15 +0800

    styling this-site page

--- app/pages/this-site.js
+++ app/pages/this-site.js
@@ -12,32 +12,36 @@ var ThisSiteModal = React.createClass({
         return (
             <Modal title="About this site" id="this-site">
                 <div className="this-site">
-                    <h3><span className="glyphicon glyphicon-cog" />Technology</h3>
-                    <dl className="dl-horizontal">
-                        <dt>Built by</dt>
-                        <dd>React.js, SCSS and webpack</dd>
-                        <dt>Framework</dt>
-                        <dd>Bootstrap UI framework</dd>
-                        <dt>Library</dt>
-                        <dd>Jquery, Vide Jquery, Font Awesome</dd>
-                    </dl>
-                    <h3><span className="glyphicon glyphicon-cog" />What bootstrap feature used</h3>
-                    <dl className="dl-horizontal">
-                        <dt>Pre & Code</dt>
-                        <dd>It is used below. The <code>pre</code> and <code>code</code> element are styled by bootstrap</dd>
-                        <dt>Horizontal description</dt>
-                        <dd>This list of bootstrap is using the <code>dl-horizontal</code> to align the title and description</dd>
-                        <dt>Gird Column System</dt>
-                        <dd>This most popular features in bootstrap is employed in the main page, under the video, to show the three emphasis of my facts</dd>
-                        <dt>Modal</dt>
-                        <dd>All the popup are using bootstrap modal actually</dd>
-                    </dl>
-
-                    <h3><span className="glyphicon glyphicon-cog" />What's new</h3>
-                    <p className="alert alert-warning" role="alert">This personal web page is based on the <a href="https://w3layouts.com/preview/?l=/easy-resume-personal-category-flat-bootstrap-responsive-web-template/">template</a> by W3layout</p>
-                    <p className="alert alert-success">All the changes can be found on Github <i className="fa fa-github"></i></p>
-                    <p>The personal website is first rebuild by React.js, and then componentize the elements. The following codeblock shows how the banner component is written:</p>
-                    <pre>{`import './banner.scss';
+                    <h3><i className="fa fa-cogs" />Technology</h3>
+                    <div className="container">
+                        <dl className="dl-horizontal">
+                            <dt>Built by</dt>
+                            <dd>React.js, SCSS and webpack</dd>
+                            <dt>Framework</dt>
+                            <dd>Bootstrap UI framework</dd>
+                            <dt>Library</dt>
+                            <dd>Jquery, Vide Jquery, Font Awesome</dd>
+                        </dl>
+                    </div>
+                    <h3><i className="fa fa-level-down" />What bootstrap feature used</h3>
+                    <div className="container">
+                        <dl className="dl-horizontal">
+                            <dt>Pre & Code</dt>
+                            <dd>It is used below. The <code>pre</code> and <code>code</code> element are styled by bootstrap</dd>
+                            <dt>Horizontal description</dt>
+                            <dd>This list of bootstrap is using the <code>dl-horizontal</code> to align the title and description</dd>
+                            <dt>Gird Column System</dt>
+                            <dd>This most popular features in bootstrap is employed in the main page, under the video, to show the three emphasis of my facts</dd>
+                            <dt>Modal</dt>
+                            <dd>All the popup are using bootstrap modal actually</dd>
+                        </dl>
+                    </div>
+                    <h3><i className="fa fa-plus-circle" />What's new</h3>
+                    <div className="container">
+                        <p className="alert alert-warning" role="alert">This personal web page is based on the <a href="https://w3layouts.com/preview/?l=/easy-resume-personal-category-flat-bootstrap-responsive-web-template/">template</a> by W3layout</p>
+                        <p className="alert alert-success">All the changes can be found on Github <i className="fa fa-github"></i></p>
+                        <p><span className="numbering">1. </span>The personal website is first rebuild by React.js, and then componentize the elements. The following codeblock shows how the banner component is written:</p>
+                        <pre>{`import './banner.scss';
 import React from 'react';
 
 var Banner = React.createClass({
@@ -57,10 +61,10 @@ var Banner = React.createClass({
     }
 });
 export default Banner;`}</pre>
-                    <p>Then to modify the css styles to scss, and makes the theme color as a variable so it can be changes easily later.</p>
-                    <p>Using react and scss makes every thing as a component, increasing the reusability of code and maintenance efficiency.</p>
-                    <p>The page is then added the three column section under the video using the bootstrap gird system</p>
-                    <p>Lastly added this explanation page and many elements in here are styled by bootstrap too!</p>
+                        <p><span className="numbering">2. </span> Then to modify the css styles to scss, and makes the theme color as a variable so it can be changes easily later. Using react and scss makes every thing as a component, increasing the reusability of code and maintenance efficiency.</p>
+                        <p><span className="numbering">3. </span> The page is then added the three column section under the video using the bootstrap gird system</p>
+                        <p><span className="numbering">4. </span> Lastly added this explanation page and many elements in here are styled by bootstrap too!</p>
+                    </div>
                 </div>
             </Modal>
         );
