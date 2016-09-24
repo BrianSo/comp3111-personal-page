a165b4ac3c027d0244fb7ae07b12c0532f6bb739
Brian So <briansostudio@gmail.com>
2016-09-24 21:54:18 +0800

    basic page content

--- app/pages/this-site.js
+++ app/pages/this-site.js
@@ -10,12 +10,22 @@ var ThisSiteModal = React.createClass({
     render: function() {
         return (
             <Modal title="About this site" id="this-site">
-                <div className="this-site">
-                    Content 123
-                    <p>para1</p>
-                    <p>para2</p>
-                    <p>para3</p>
-                </div>
+                <h2>technology</h2>
+                <p>Built by: React.js, SCSS and webpack</p>
+                <p>Framework: Bootstrap UI framework</p>
+                <p>Library: Jquery, Vide Jquery</p>
+                <h2>What bootstrap feature used</h2>
+                <p>Code</p>
+                <p>Gird Column System</p>
+                <p>Modal</p>
+                <h2>What is new</h2>
+                <p>This personal web page is based on the <a href="https://w3layouts.com/preview/?l=/easy-resume-personal-category-flat-bootstrap-responsive-web-template/">template</a> by W3layout</p>
+                <p>All the changes can be found on Github</p>
+                <p>rebuild by React.js, componentize</p>
+                <p>Overall style</p>
+                <p>This explanation page</p>
+                <p>Three column</p>
+
             </Modal>
         );
     }
